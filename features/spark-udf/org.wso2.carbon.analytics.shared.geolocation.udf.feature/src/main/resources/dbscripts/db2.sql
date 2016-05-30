25ebfdc6e421b63707d3455f257dcd6eae18ff2d
Tharindu Dharmarathne <tharindua@wso2.com>
2016-05-30 11:20:14 +0530

    commiting changes on script chnges

--- features/spark-udf/org.wso2.carbon.analytics.shared.geolocation.udf.feature/src/main/resources/dbscripts/db2.sql
+++ features/spark-udf/org.wso2.carbon.analytics.shared.geolocation.udf.feature/src/main/resources/dbscripts/db2.sql
@@ -1,4 +1,5 @@
 CREATE TABLE BLOCKS (
+network_cidr varchar(45) DEFAULT NULL,
 network BIGINT DEFAULT NULL,
   broadcast BIGINT DEFAULT NULL,
  geoname_id BIGINT DEFAULT NULL,
@@ -8,10 +9,12 @@ network BIGINT DEFAULT NULL,
   is_satellite_provider SMALLINT DEFAULT '0',
   postal_code VARCHAR(45) DEFAULT NULL,
   latitude DECIMAL(10,4) DEFAULT NULL,
-  longitude DECIMAL(10,4) DEFAULT NULL)/
+  longitude DECIMAL(10,4) DEFAULT NULL,
+network_blocks varchar(45) DEFAULT NULL)/
 
 CREATE INDEX idx_blocks_network ON BLOCKS (network)/
 CREATE INDEX idx_blocks_broadcast ON BLOCKS (broadcast)/
+CREATE INDEX idx_blocks_network_blocks ON BLOCKS (network_blocks)/
 CREATE TABLE LOCATION (
   geoname_id BIGINT NOT NULL,
   locale_code VARCHAR(10) DEFAULT NULL,
--- features/spark-udf/org.wso2.carbon.analytics.shared.geolocation.udf.feature/src/main/resources/dbscripts/h2.sql
+++ features/spark-udf/org.wso2.carbon.analytics.shared.geolocation.udf.feature/src/main/resources/dbscripts/h2.sql
@@ -1,4 +1,5 @@
 CREATE TABLE `BLOCKS` (
+  `network_cidr` varchar(45) DEFAULT NULL,
 `network` long unsigned DEFAULT NULL,
   `broadcast` long unsigned DEFAULT NULL,
  `geoname_id` int(11) DEFAULT NULL,
@@ -8,10 +9,11 @@ CREATE TABLE `BLOCKS` (
   `is_satellite_provider` tinyint(1) DEFAULT '0',
   `postal_code` varchar(45) DEFAULT NULL,
   `latitude` float DEFAULT NULL,
-  `longitude` float DEFAULT NULL);
+  `longitude` float DEFAULT NULL),
+  `network_blocks` varchar(45) DEFAULT NULL);
 CREATE INDEX idx_blocks_network ON BLOCKS (`network`);
 CREATE INDEX idx_blocks_broadcast ON BLOCKS (`broadcast`);
-
+CREATE INDEX idx_blocks_network_blocks ON BLOCKS (`network_blocks`);
 CREATE TABLE `LOCATION` (
   `geoname_id` int(10) unsigned NOT NULL,
   `locale_code` varchar(10) DEFAULT NULL,
@@ -36,6 +38,6 @@ CREATE TABLE `IP_LOCATION` (
 );
 
 INSERT INTO LOCATION VALUES (2803476,'en','EU','Europe','DE','Germany','SN','Saxony','','','Zwoenitz',0,'Europe/Berlin');
-INSERT INTO BLOCKS VALUES (3232236032, 3232236287, 2803476,2803476,2803476,1,1,'1',1,1);
+INSERT INTO BLOCKS VALUES ('192.168.2.0/24',3232236032, 3232236287, 2803476,2803476,2803476,1,1,'1',1,1,'192.168');
 
 
