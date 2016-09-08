f46c4b8cfefa9c80d18c7e7eefd0296d7671e216
mohan <mohan.vive@gmail.com>
2016-09-08 10:11:38 +0530

    updating the getCityFromIP function

--- components/spark-udf/org.wso2.carbon.analytics.shared.geolocation.udf/src/main/java/org/wso2/carbon/analytics/shared/geolocation/impl/GeoLocationResolverUDFWithImprovedCache.java
+++ components/spark-udf/org.wso2.carbon.analytics.shared.geolocation.udf/src/main/java/org/wso2/carbon/analytics/shared/geolocation/impl/GeoLocationResolverUDFWithImprovedCache.java
@@ -70,6 +70,8 @@ public class GeoLocationResolverUDFWithImprovedCache implements CarbonUDF {
             location = GeoResolverInitializer.getInstance().getLocationResolver().getLocation(ip);
             if (location != null) {
                 locationLRUCache.put(ip, location);
+            } else {
+                locationLRUCache.put(ip, new Location("", null, null));
             }
         }
         return location != null ? location.getCity() : "";
