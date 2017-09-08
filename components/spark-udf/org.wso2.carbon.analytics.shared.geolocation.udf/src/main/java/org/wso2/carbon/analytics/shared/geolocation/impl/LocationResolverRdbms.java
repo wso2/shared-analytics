6c9962bb95c0b08c8c31eec5fba98d50a3de0c95
Ruwan Abeykoon <ruwana@wso2.com>
2017-09-08 15:05:31 +0530

    Fix the issues [1] and [2], and unit tests added. [1] https://github.com/wso2/shared-analytics/issues/151 [2] https://github.com/wso2/shared-analytics/issues/150

--- components/spark-udf/org.wso2.carbon.analytics.shared.geolocation.udf/src/main/java/org/wso2/carbon/analytics/shared/geolocation/impl/LocationResolverRdbms.java
+++ components/spark-udf/org.wso2.carbon.analytics.shared.geolocation.udf/src/main/java/org/wso2/carbon/analytics/shared/geolocation/impl/LocationResolverRdbms.java
@@ -17,18 +17,24 @@
 */
 package org.wso2.carbon.analytics.shared.geolocation.impl;
 
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
-import org.wso2.carbon.analytics.shared.geolocation.api.LocationResolver;
 import org.wso2.carbon.analytics.shared.geolocation.api.Location;
+import org.wso2.carbon.analytics.shared.geolocation.api.LocationResolver;
 import org.wso2.carbon.analytics.shared.geolocation.dbutil.DBUtil;
 import org.wso2.carbon.analytics.shared.geolocation.exception.GeoLocationResolverException;
 
+import java.net.Inet4Address;
+import java.net.Inet6Address;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Map;
+import java.util.WeakHashMap;
+import java.util.concurrent.locks.Lock;
 
 public class LocationResolverRdbms extends LocationResolver {
     private static final Log log = LogFactory.getLog(LocationResolverRdbms.class);
@@ -119,33 +125,83 @@ public class LocationResolverRdbms extends LocationResolver {
     private Location getLocationFromIp(String ipAddress) throws GeoLocationResolverException {
         Location location = null;
         Connection connection = null;
-        PreparedStatement statement = null;
-        ResultSet resultSet = null;
         try {
             connection = dbUtil.getConnection();
+            if (persistInDataBase) {
+                location = loadLocation(ipAddress, connection);
+            }
+            if (location == null) {
+                InetAddress address = InetAddress.getByName(ipAddress);
+
+                if (address instanceof Inet6Address) {
+                    // It's ipv6
+                    // Any mapped IPv4 address in IPv6 space will also returns as Inet4Address
+                    if (log.isDebugEnabled()) {
+                        log.debug(
+                                "Found IPv6 address which can not be resolved to location. IP Address = " + ipAddress);
+                    }
+                    location = getLocationFromIPv6((Inet6Address) address, connection);
+                } else if (address instanceof Inet4Address) {
+                    // It's ipv4
+                    location = getLocationFromLongValueOfIp(address.getHostAddress(), connection);
+                }
+
+                if (location != null) {
+                    if (persistInDataBase) {
+                        //Insert or update in Application Level, Rather than using DB specific query.
+                        boolean autoCommitMode = connection.getAutoCommit();
+                        try {
+                            connection.setAutoCommit(false);
+                            Location updatedLocation = loadLocation(location.getIp(), connection);
+                            if (updatedLocation != null) {
+                                saveLocation(location, connection);
+                            }
+                        } finally {
+                            connection.setAutoCommit(autoCommitMode);
+                        }
+                    }
+                }
+            }
+        } catch (UnknownHostException e) {
+            throw new GeoLocationResolverException("Error while parsing the IP address : " + ipAddress, e);
+        } catch (SQLException e) {
+            throw new GeoLocationResolverException("Error while getting the location from database", e);
+        } finally {
+            dbUtil.closeAllConnections(null, connection, null);
+        }
+        return location;
+    }
+
+    /**
+     * Calls external system or database database to find the IPv6 adress to location details.
+     * Can be used by an extended class.
+     *
+     * @param address
+     * @param connection the Db connection to be used. Do not close this connection within this method.
+     * @return
+     */
+    protected Location getLocationFromIPv6(Inet6Address address, Connection connection)
+            throws SQLException, GeoLocationResolverException {
+        return null;
+    }
+
+
+
+    private Location loadLocation(String ipAddress, Connection connection) throws SQLException {
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+        Location location = null;
+        try {
             if (persistInDataBase) {
                 statement = connection.prepareStatement(SQL_SELECT_LOCATION_FROM_IP);
                 statement.setString(1, ipAddress);
                 resultSet = statement.executeQuery();
             }
             if (resultSet != null && resultSet.next()) {
-                location = new Location(resultSet.getString("country_name"), resultSet.getString("city_name"),
-                        ipAddress);
-            } else {
-                location = getLocationFromLongValueOfIp(ipAddress, connection);
-                if (location != null) {
-                    if(location.getCity() == null){
-                        location.setCity("");
-                    }
-                    if (persistInDataBase) {
-                        saveLocation(location, connection);
-                    }
-                }
+                location = new Location(resultSet.getString("country_name"), resultSet.getString("city_name"), ipAddress);
             }
-        } catch (SQLException e) {
-            throw new GeoLocationResolverException("Error while getting the location from database", e);
-        } finally {
-            dbUtil.closeAllConnections(statement, connection, resultSet);
+        }finally {
+            dbUtil.closeAllConnections(statement, null, resultSet);
         }
         return location;
     }
